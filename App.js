381f4e7cc4674baf86ede19366a7982bcea673b9
L XB <1169959173@qq.com>
2020-07-09 18:09:19 +0800

    2020-7-9 18:08:36: rn-native文档1-4

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-native/no-inline-styles */
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
@@ -6,7 +7,7 @@
  * @flow strict-local
  */
 import 'react-native-gesture-handler';
-import React from 'react';
+import React, { useState } from 'react';
 import {
   SafeAreaView,
   StyleSheet,
@@ -15,137 +16,203 @@ import {
   Text,
   StatusBar,
   TouchableHighlight,
+  TextInput,
   Alert,
-  Button,
+  TouchableOpacity,
+  Button, // 不能自定义样式
 } from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
+/** 首屏 */
 function HomeScreen({ navigation, route }) {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Button
-        onPress={() => navigation.navigate('Details')}
-        title="Go to Detailes"
-      />
-      <Button
-        onPress={() => navigation.navigate('Page1')}
-        title="Go to Page1"
-      />
+    <View style={styles.screen}>
+      <View style={styles.btnWrapper}>
+        <TouchableOpacity
+          style={styles.btn}
+          onPress={() =>
+            navigation.navigate('Details', {
+              id: Math.floor(Math.random() * 10),
+            })
+          }
+        >
+          <Text>Go to Detailes</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.btn}
+          onPress={() => navigation.navigate('Page1')}
+        >
+          <Text>Go to Page1</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
 
 function Page1({ navigation, route }) {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Button onPress={() => navigation.goBack()} title="goBack" />
-      <Button onPress={() => navigation.popToTop()} title="Go to popTop" />
+    <View style={styles.screen}>
+      <View style={styles.btnWrapper}>
+        <TouchableOpacity
+          style={styles.btn}
+          onPress={() => navigation.goBack()}
+        >
+          <Text>goBack</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.btn}
+          onPress={() => navigation.popToTop()}
+        >
+          <Text>Go to popTop</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
 
 function Page2({ navigation, route }) {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <View style={styles.screen}>
       <View style={styles.btnWrapper}>
-        <Button onPress={() => navigation.goBack()} title="goBack" />
-        <Button onPress={() => navigation.popToTop()} title="Go to popTop" />
+        <TouchableOpacity
+          style={styles.btn}
+          onPress={() => navigation.goBack()}
+        >
+          <Text>goBack</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.btn}
+          onPress={() => navigation.popToTop()}
+        >
+          <Text>Go to popTop</Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
 }
 
-const DetailsScreen = React.memo(({ navigation, route }) => {
+const CreateMessage = ({ navigation }) => {
+  const [message, setMessage] = useState('');
+
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}
-        >
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>编写代码</Text>
-              <View style={styles.btnWrapper}>
-                <TouchableHighlight
-                  style={{
-                    flex: 1,
-                    backgroundColor: '#1890ff',
-                    padding: 4,
-                    textAlign: 'center',
-                  }}
-                  onPress={() => navigation.navigate('Home')}
-                >
-                  <Text>Go to Home</Text>
-                </TouchableHighlight>
-                <Button
-                  style={{
-                    flex: 1,
-                    backgroundColor: '#1890ff',
-                    padding: 4,
-                    textAlign: 'center',
-                  }}
-                  onPress={() => navigation.navigate('Page2')}
-                  title="Go to Page2"
-                />
-              </View>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <TextInput
+        multiline
+        placeholder="告诉我你的想法"
+        style={{ height: 200, padding: 10, backgroundColor: 'white' }}
+        value={message}
+        onChangeText={setMessage}
+      />
+      <Button
+        title="留言"
+        onPress={() => {
+          navigation.navigate('Home', { post: message });
+        }}
+      />
+    </View>
+  );
+};
+
+const DetailsScreen = React.memo(({ navigation, route }) => {
+  const { id } = route.params;
+  return (
+    <SafeAreaView style={styles.screen}>
+      <ScrollView contentInsetAdjustmentBehavior="automatic">
+        <View>
+          <View style={styles.sectionContainer}>
+            <Text style={styles.sectionDescription}>id: {id}</Text>
+          </View>
+          <View style={styles.sectionContainer}>
+            <View style={styles.btnWrapper}>
+              <TouchableOpacity
+                style={styles.btn}
+                onPress={() =>
+                  navigation.setParams({ id: Math.floor(Math.random() * 10) })
+                }
+              >
+                <Text>更新id</Text>
+              </TouchableOpacity>
             </View>
           </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
+          <View style={styles.sectionContainer}>
+            <View style={styles.btnWrapper}>
+              <TouchableOpacity
+                style={styles.btn}
+                onPress={() => navigation.navigate('Home')}
+              >
+                <Text>Go to Home</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={styles.btn}
+                onPress={() => navigation.navigate('Page2')}
+              >
+                <Text>Go to Page2</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+          <View style={styles.sectionContainer}>
+            <View style={styles.btnWrapper}>
+              <TouchableOpacity
+                style={styles.btn}
+                onPress={() => navigation.navigate('Message')}
+              >
+                <Text>留言</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 });
 
 const Stack = createStackNavigator();
 
-const App: () => React$Node = () => {
+const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Details" component={DetailsScreen} />
+      <Stack.Navigator
+        initialRouteName="Home"
+        initialOptions={{}}
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: '#1890ff',
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+        }}
+      >
+        <Stack.Screen
+          name="Details"
+          component={DetailsScreen}
+          initialParams={{ id: 1 }} // 默认参数
+          options={({ route }) => ({ title: `${route.params.id} Details` })}
+        />
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Page1" component={Page1} />
         <Stack.Screen name="Page2" component={Page2} />
+        <Stack.Screen
+          name="Message"
+          component={CreateMessage}
+          options={{ title: '留言信箱' }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
 };
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
+  screen: {
+    flex: 1,
+    paddingLeft: 5,
+    paddingRight: 5,
+    backgroundColor: '#f7f7f7',
   },
   sectionContainer: {
     marginTop: 32,
@@ -157,27 +224,26 @@ const styles = StyleSheet.create({
     color: Colors.black,
   },
   sectionDescription: {
-    marginTop: 8,
     fontSize: 18,
     fontWeight: '400',
     color: Colors.dark,
   },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
   btnWrapper: {
-    flex: 1,
     flexDirection: 'row',
     justifyContent: 'space-around',
+  },
+  btn: {
+    backgroundColor: '#1890ff',
+    borderRadius: 5,
+    color: '#fff',
+    fontSize: 16,
+    padding: 3,
+    height: 43,
+    justifyContent: 'center',
     alignItems: 'center',
+    width: 100,
+    flexWrap: 'nowrap',
+    overflow: 'hidden',
   },
 });
 
